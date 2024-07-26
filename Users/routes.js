6a70ce1e6e2d8d097e20679b91df988679c26101
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-07-26 13:15:33 +0530

    working code till profile

--- Users/routes.js
+++ Users/routes.js
@@ -34,9 +34,15 @@ export default function UserRoutes(app) {
     res.json(status);
   };
   const signup = async (req, res) => { };
-  const signin = async (req, res) => { };
+  const signin = async (req, res) => {
+    const { username, password } = req.body;
+    currentUser = await dao.findUserByCredentials(username, password);
+    res.json(currentUser);
+  };
   const signout = (req, res) => { };
-  const profile = async (req, res) => { };
+  const profile = async (req, res) => {
+    res.json(currentUser);
+  };
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
