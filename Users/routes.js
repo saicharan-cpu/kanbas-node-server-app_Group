e54b939cd83c94c7ca60b09b62c1f84b0a2fbcf4
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-07-28 16:48:56 +0530

    working code including 5.7

--- Users/routes.js
+++ Users/routes.js
@@ -1,5 +1,5 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+
 export default function UserRoutes(app) {
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
@@ -35,8 +35,13 @@ export default function UserRoutes(app) {
   };
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
   };
   const signup = async (req, res) => {
     const user = await dao.findUserByUsername(req.body.username);
@@ -45,14 +50,20 @@ export default function UserRoutes(app) {
         { message: "Username already taken" });
       return;
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
   const signout = (req, res) => {
-    currentUser = null;
+    req.session.destroy();
     res.sendStatus(200);
   };
-  const profile = async (req, res) => {
+  const profile = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
   app.post("/api/users", createUser);
