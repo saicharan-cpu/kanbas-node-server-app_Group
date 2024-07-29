e6012e9f8bbd3a46df670c5fed80f578bc4a0fb7
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-07-29 10:29:38 +0530

    working code till courses

---
+++ Kanbas/Courses/schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  _id: { type: String, required: false },
+  name: { type: String, required: true },
+  number: { type: String, required: false },
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: true },
+  department: { type: String, required: false },
+  credits: { type: Number, required: false },
+  description: { type: String, required: true }
+}, { collection: "courses" });
+
+export default courseSchema;
