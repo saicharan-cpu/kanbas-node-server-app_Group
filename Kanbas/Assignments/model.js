eac53d80c95a0df900c24f693436d248ead19ce7
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-07-30 21:08:08 +0530

    working changes for assignments

---
+++ Kanbas/Assignments/model.js
@@ -0,0 +1,6 @@
+// Kanbas/Assignments/model.js
+import mongoose from 'mongoose';
+import assignmentSchema from './schema.js';
+
+const AssignmentModel = mongoose.model('AssignmentModel', assignmentSchema);
+export default AssignmentModel;
