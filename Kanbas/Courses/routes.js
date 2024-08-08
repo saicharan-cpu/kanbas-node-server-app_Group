01eb3fde4f1d88ced4aaa9260481ab225745a79c
Sai Charan Reddy <saicharanreddy@Sais-MacBook-Air.local>
2024-08-08 13:24:07 +0530

    working code changes for showing only enrolled courses and allowing enrollement

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export default function CourseRoutes(app) {
   const createCourse = async (req, res) => {
     let course = req.body;
-      course._id = new mongoose.Types.ObjectId().toString();
+    course._id = new mongoose.Types.ObjectId().toString();
     const newCourse = await dao.createCourse(course);
     res.json(newCourse);
   };
@@ -17,7 +17,7 @@ export default function CourseRoutes(app) {
   const findAllCourses = async (req, res) => {
     console.log("Fetching all courses");
     const courses = await dao.findAllCourses();
-    console.log("Fetched courses:"+courses);
+    console.log("Fetched courses:" + JSON.stringify(courses));
     res.json(courses);
   };
 
@@ -32,9 +32,28 @@ export default function CourseRoutes(app) {
     res.json(status);
   };
 
+  const enrollInCourse = async (req, res) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    console.log("In server enroll in course: "+userId+" course id is: "+id);
+    try {
+      const course = await dao.findCourseById(id);
+      if (!course.enrolled.includes(userId)) {
+        course.enrolled.push(userId);
+        const updatedCourse = await dao.updateCourse(id, course);
+        res.json(updatedCourse);
+      } else {
+        res.status(400).json({ message: 'User already enrolled' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to enroll in course', error });
+    }
+  };
+
   app.post("/api/courses", createCourse);
   app.get("/api/courses", findAllCourses);
   app.get("/api/courses/:id", findCourseById);
   app.put("/api/courses/:id", updateCourse);
   app.delete("/api/courses/:id", deleteCourse);
+  app.post("/api/courses/:id/enroll", enrollInCourse);
 }
